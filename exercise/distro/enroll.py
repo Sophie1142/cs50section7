4e62063ce5ae7862e37dfc39de86184479e3be37
Sophie Edouard <sedouard@college.harvard.edu>
2020-10-20 19:02:37 +0000

    presection

---
+++ exercise/distro/enroll.py
@@ -0,0 +1,3 @@
+# Write a Python program enroll.py to add new students
+# to the roster and enroll them in courses.
+# You can use get_string to get user input
---
+++ exercise/distro/questions.sql
@@ -0,0 +1,14 @@
+-- What is Alice's student id?
+
+-- What is the course title for CS51?
+
+-- What are the course codes and titles for all of the CS courses?
+-- (Assume that all CS courses have a course code that begins with 'CS')
+
+-- How many courses are there?
+
+-- How many students are taking CS50?
+
+-- What are the names of all of the instructors? Output 2 columns with all instructors' names and the course they teach.
+
+-- What are the names of all of the students taking CS50?
